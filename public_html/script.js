4525f5d04217075a63123fc73e7cbc1172a1c811
marzz11 <126739101+marzz11@users.noreply.github.com>
2025-01-29 15:25:53 -0500

    Delete public_html directory

--- public_html/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.getElementById('theme-toggle');
-    
-    themeToggleButton.addEventListener('click', () => {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-    });
-});
